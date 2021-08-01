907a08f0c86adce195406375dbf17c980af63943
tripathi221 <laxmikant.tripathi6@gmail.com>
2021-08-01 14:10:24 +0530

    create database connection.

--- todo-backend/main.py
+++ todo-backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
 
+# database connection
+DB_URL = 'mongodb://localhost:27017'
+DB_NAME = 'task-manager'
+
+@app.on_event("startup")
+async def start_db_client():
+    app.mongodb_client = AsyncIOMotorClient(DB_URL)
+    app.mongodb = app.mongodb_client[DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
 @app.get("/")
 async def main():
+    import pdb; pdb.set_trace()
     return {"Hello": "World"}
+
